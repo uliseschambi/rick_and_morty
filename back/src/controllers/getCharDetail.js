58b3bc6a0ce8a984c5f1561eae397feb3fbe050f
uliseschambi <uliseschambi@gmail.com>
2023-03-28 01:01:04 -0300

    express

--- back/src/controllers/getCharDetail.js
+++ back/src/controllers/getCharDetail.js
@@ -22,27 +22,4 @@ const getCharDetail = (req, res) => {
     });
 };
 
-// const getCharDetail = (res, ID) => {
-//   axios
-//     // .get(`https://rickandmortyapi.com/api/character/${ID}`)
-//     .get(`${URL}/character/${ID}?key=${KEY}`) // api henry
-//     .then(({ data }) => {
-//       res.writeHead(200, { 'Content-Type': 'application/json' });
-//       res.end(
-//         JSON.stringify({
-//           id: data.id,
-//           image: data.image,
-//           name: data.name,
-//           gender: data.gender,
-//           status: data.status,
-//           origin: data.origin,
-//           species: data.species,
-//         })
-//       );
-//     })
-//     .catch(error => {
-//       res.writeHead(500, { 'Content-Type': 'text/plain' }).end(error.message);
-//     });
-// };
-
 module.exports = getCharDetail;
